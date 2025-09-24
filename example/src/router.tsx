b1de2bb59a2a89b45c74e5e64d5e30a94c2195f9
Nick Nisi <nick.nisi@workos.com>
2025-09-24 09:15:19 -0500

    fix .env loading

--- example/src/router.tsx
+++ example/src/router.tsx
@@ -1,8 +1,6 @@
 import { createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 
-console.log('ROUTER', process.env.WORKOS_API_KEY);
-
 export function getRouter() {
   const router = createRouter({
     routeTree,
